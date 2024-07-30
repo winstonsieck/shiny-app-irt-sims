db0419741628bf379378a6f60dda5eaffea7ddfb
winstonsieck <sieck@globalcognition.org>
2024-07-30 14:05:20 -0400

    add theme, html. replace num inputs with sliders and dropdown

--- app.R
+++ app.R
@@ -2,29 +2,46 @@
 
 
 # The UI
+library(shinythemes)
+
 
 ui <- fluidPage(
+  #theme = shinytheme("superhero"),
+                
+  titlePanel(
+    div(style="margin-bottom:50px;",
+      h1("Rasch Model Simulations", align="center")
+    )),
+  
   fluidRow(
     column(4, 
-           "Test Taker Distribution",
-           numericInput("np", label = "Number of People", value = 500, min = 1),
+           tags$h4("Sample Sizes"),
+#           selectInput("np", label = "Number of People", c(500,750,1000)),
+           sliderInput("np", "Number of People", value = 500, min = 500, max = 1000, step=50),
+           sliderInput("ni", "Number of Items", value = 50, min = 10, max = 1000, step=10),
+#           numericInput("ni", label = "Number of Items", value = 50, min = 1),
     ),
     column(4, 
-           "Item Distribution",
-           numericInput("ni", label = "Number of Items", value = 50, min = 1),
-           numericInput("hard", label = "Hardness", value = 2, min = 0, step = 0.1),
-           numericInput("easy", label = "Easiness", value = 2, min = 0, step = 0.1)
+           tags$h4("Item Distribution"),
+           selectInput("hard", label = "Hardness", c(0,.25,.5,1,2,4,16,256), selected=4),
+           selectInput("easy", label = "Easiness", c(0,.25,.5,1,2,4,16,256), selected=1)
+#           numericInput("hard", label = "Hardness", value = 2, min = 0, step = 0.1),
+#           numericInput("easy", label = "Easiness", value = 2, min = 0, step = 0.1)
     ),
     column(4,
-           "Plots",
-# placeholder for plot controls
+           tags$h4("Display Settings"),
+           actionButton("simulate", "Run Simulation")
     )
   ),
-  fluidRow(
-    column(9, plotOutput("testinfo")),
-    column(3, verbatimTextOutput("raschmodel"))
+
+  mainPanel(
+    tabsetPanel(
+      tabPanel("Test Info Curve", plotOutput("testinfo")),
+      tabPanel("Model Details", verbatimTextOutput("raschmodel"))
+    )
   )
 )
+
 
 
 # Server side
@@ -55,12 +72,26 @@ server <- function(input, output, session) {
   }
   
   # output rendering  
-  theta_true <- reactive(rnorm(input$np,0,1))
-  b_true <- reactive(6*(rbeta(input$ni,input$hard,input$easy)-.5))
+  v <- reactiveValues(x=NULL)
+  observeEvent(input$np, {v$x <- as.numeric(input$np)} )
+  observeEvent(v$x, {if(v$x != as.numeric(input$ni)){
+    updateSliderInput(session,"ni", max = .1*v$x)
+  }  })
+    
+  np <- eventReactive(input$simulate, as.numeric(input$np))
+  ni <- eventReactive(input$simulate, as.numeric(input$ni))
+  hard <- eventReactive(input$simulate, as.numeric(input$hard))
+  easy <- eventReactive(input$simulate, as.numeric(input$easy))
   
-  df <- reactive(sim_test_dat(input$np,input$ni,theta_true(),b_true()))
+  theta_true <- eventReactive(input$simulate, { rnorm(np(),0,1) })
+  b_true <- eventReactive(input$simulate, {
+    6*(rbeta(ni(),hard(),easy())-.5) })
   
-  rasch.m <- reactive(mirt(df(), 1, itemtype = "Rasch", verbose = F))
+  df <- eventReactive(input$simulate, {
+    sim_test_dat(np(),ni(),theta_true(),b_true()) })
+  
+  rasch.m <- eventReactive(input$simulate, {
+    mirt(df(), 1, itemtype = "Rasch", verbose = F) })
   
   output$testinfo <- renderPlot({
     testInfoPlot(rasch.m(), adj_factor = 2)
